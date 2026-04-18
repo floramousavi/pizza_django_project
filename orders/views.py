a5e06aa06d747feadfcaeae66d54eeb1c91c3a6d
floramousavi <s2413078@bi.no>
2026-04-18 22:01:18 +0200

    created the order history to record the previous orders of customers.

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from .models import Order
 
-# Create your views here.
+
+@login_required
+def order_history(request):
+    orders = Order.objects.filter(user=request.user).order_by("-created_at")
+    return render(request, "orders/order_history.html", {"orders": orders})
