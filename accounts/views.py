01e3c053057515425d1faa2eb315a5fa47678a86
floramousavi <s2413078@bi.no>
2026-04-18 12:09:01 +0200

    created the authentication including registration,login/logout.

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import redirect, render
 
-# Create your views here.
+
+def register_view(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, "Your account has been created successfully.")
+            return redirect("menu:home")
+    else:
+        form = UserCreationForm()
+
+    return render(request, "accounts/register.html", {"form": form})
