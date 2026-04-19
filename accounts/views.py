a36de895180597ada6c181cb229226fb1c2986aa
floramousavi <s2413078@bi.no>
2026-04-19 13:06:48 +0200

    Improved the registration form to collect first name, last name and email.

--- accounts/views.py
+++ accounts/views.py
@@ -1,18 +1,19 @@
 from django.contrib import messages
 from django.contrib.auth import login
-from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import redirect, render
+
+from .forms import CustomUserRegistrationForm
 
 
 def register_view(request):
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = CustomUserRegistrationForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
             messages.success(request, "Your account has been created successfully.")
             return redirect("menu:home")
     else:
-        form = UserCreationForm()
+        form = CustomUserRegistrationForm()
 
     return render(request, "accounts/register.html", {"form": form})
